4a38342e8c770a3e35783e57f89b574d23493e35
Shahid Rauf <shahidrauf.141@gmail.com>
2019-11-28 13:24:00 +0500

    task is done by submit function

--- src/Form.js
+++ src/Form.js
@@ -1,31 +1,21 @@
 import React,{useState} from 'react';
 import  doIt from "./calculator.js"
-      
- 
- 
+       
 const Form = () => {
    const [output, setOutput]= useState(0);
-   // function callF(event){
-   //    //  console.log(")))))))))", event.target.firstvalue.value, event.target.secondvalue.value)
-   //     event.preventDefault();
-   //     const a = parseInt(event.target.firstvalue.value);
-   //     const b = parseInt(event.target.secondvalue.value);
-   //     const result = doIt(a,b,"+");
-   //     setOutput(result);
-   //    }
+   
       function perfAction(event){
-         //  console.log(")))))))))", event.target.firstvalue.value, event.target.secondvalue.value)
           event.preventDefault();
           const a = parseInt(event.target.firstvalue.value);
           const b = parseInt(event.target.secondvalue.value);
           const res = event.target.arithM.value;
-          const result = doIt(a,b,res);
+          const result = doIt(a,b, res);
           setOutput(result);
          }
- 
+
     return (
         
-          <form name="myForm" >
+          <form name="myForm" onSubmit={perfAction} >
              <p>Enter first value:</p>
              <input
              type="text"
@@ -35,16 +25,16 @@ const Form = () => {
              <input
              type="text"
              name="secondvalue"
-             /> <br /> <br /> 
-             {/* <button type ="submit"> Click to get Answer!</button> */}
-             <select name="arithM" onChange={perfAction}>
-                <option >Select One</option>
+             />  
+              <br /> <br />
+             <select name="arithM"   >
                 <option value="+">+</option>
                 <option value="-">-</option>
                 <option value="*">*</option>
                 <option value="/">/</option>
-                 
              </select>
+             <br />
+             <button type ="submit"> Click to get Answer!</button> 
              <br /> <br />
              <div> Output is : {output} </div>
           </form>
