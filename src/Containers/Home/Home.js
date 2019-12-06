8daf9a512d2d00e92cd2a693ac264ad749533510
Shahid Rauf <shahidrauf.141@gmail.com>
2019-12-06 18:39:28 +0500

    after merge with routing!

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { useHistory } from "react-router-dom";
 export default function() {
+  function HomeButton() {
+    let history = useHistory();
+  
+    function handleClick() {
+      history.push("/home");
+    }
+  }
   return (
-    <div>This is Home</div>
+    <div>
+      {/* This is Home */}
+      <button type="button" onClick={handleClick}>
+      Go home
+    </button>
+    </div>
+    
   )
 }
--- src/Routing/Routes.js
+++ src/Routing/Routes.js
@@ -4,11 +4,14 @@ import {
   Route
 } from 'react-router-dom'
 import Home from '../Containers/Home/Home'
+import Home from '../Containers/Calculator/calculator'
 
 export default function() {
   return (
     <Switch>
       <Route path="/" exact><Home /></Route>
+      <Route path="/calulator" exact><calculator /></Route>
+
     </Switch>
   )
 }
