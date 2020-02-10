0606e6aa256ccaa16067283ce4a8a6654a0e918c
AhmedKhan64 <makhan3064@gmail.com>
2020-02-10 14:20:02 +0500

    Redux corrected

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Router>
       <Routes />
-    </Router>
+    </Router>>>>>
   );
 };
 
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,35 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import { useHistory } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import styles from '../DiceRoller/DiceRoller.module.scss';
-
-export default function() {
-  const history = useHistory();
-  const Click = () => {
-    history.push('/dice-roller');
-  };
-  const Profiles = () => {
-    history.push('/profile-class');
-  };
-  const Fetch = () => {
-    history.push('/ui-table');
-  };
-
-  return (
-    <div className={styles.click}>
-      Welcome to Home <br />
-      <Button variant="contained" color="secondary" onClick={Click}>
-        Click To Play
-      </Button>
-      <br />
-      <Button variant="contained" onClick={Profiles}>
-        Click To View Profiles
-      </Button>
-      <Button variant="contained" onClick={Fetch}>
-        Check Fetch Data
-      </Button>
-=======
 import { useSelector, useDispatch } from 'react-redux';
 import { Actions } from '../../store/actions/home';
 
@@ -47,7 +16,6 @@ export default function() {
       <button onClick={changeText} type="button">
         Changer
       </button>
->>>>>>> 33c953a7fa3a6466e1e1cce987d4ee87605cf3e3
     </div>
   );
 }
