6d21edd3c485a408ca5cf86b0d01a3a14919c0d8
AhmedKhan64 <makhan3064@gmail.com>
2020-02-11 12:21:02 +0500

    crud-redux configured

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
-<<<<<<< HEAD
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from '../DiceRoller/DiceRoller.module.scss';
+import { Actions } from '../../store/actions/home';
 
 export default function() {
+  const HomeState = useSelector(state => state.Home);
+  const dispatch = useDispatch();
+  const changeText = () => {
+    dispatch(Actions.setWelcomeText('New Text'));
+  };
+
   const history = useHistory();
   const Click = () => {
     history.push('/dice-roller');
@@ -19,6 +26,10 @@ export default function() {
   return (
     <div className={styles.click}>
       Welcome to Home <br />
+      <div>Hello: {HomeState.welcomeText}</div>
+      <button onClick={changeText} type="button">
+        Changer
+      </button>
       <Button variant="contained" color="secondary" onClick={Click}>
         Click To Play
       </Button>
@@ -29,25 +40,6 @@ export default function() {
       <Button variant="contained" onClick={Fetch}>
         Check Fetch Data
       </Button>
-=======
-import { useSelector, useDispatch } from 'react-redux';
-import { Actions } from '../../store/actions/home';
-
-export default function() {
-  const HomeState = useSelector(state => state.Home);
-  const dispatch = useDispatch();
-
-  const changeText = () => {
-    dispatch(Actions.setWelcomeText('New Text'));
-  };
-
-  return (
-    <div>
-      <div>Hello: {HomeState.welcomeText}</div>
-      <button onClick={changeText} type="button">
-        Changer
-      </button>
->>>>>>> 33c953a7fa3a6466e1e1cce987d4ee87605cf3e3
     </div>
   );
 }
