4f7632d8f4a42b926e4dc7e6ea19427e22438faf
SAMI KHAN <bcsf17a033@pucit.edu.pk>
2020-04-05 19:15:46 +0500

    Fetch-and-Display-using-class-component

--- src/Containers/FetchData/DisplayData.js
+++ src/Containers/FetchData/DisplayData.js
@@ -1,31 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, {Component} from 'react'
 import style from '../../guessnumber.module'
-const Display = ({data}) =>{
-    const backToHome = ()=>{
-
+class Display extends Component
+{
+    render()
+    {
+        const data = this.props.props;
+        return(
+        <div> 
+        {
+            data.map(dataposts =>{
+                const {id, title, thumbnailUrl} = dataposts;
+                return(
+                    <div
+                    key={id}
+                    className={style.imagecover}
+                    >
+                        <img src={thumbnailUrl} alt='image' className={style.image}/>
+                        <p className={style.title}>{title}</p>
+                    </div>
+                )
+            })
+        }
+        </div>
+        )
     }
-
-    return(
-    <div> 
-         
-      {
-          
-          data.map(dataposts =>{
-              const {id, title, thumbnailUrl} = dataposts;
-              return(
-                  <div
-                  key={id}
-                  className={style.imagecover}
-                  >
-                      <img src={thumbnailUrl} alt='image' className={style.image}/>
-                      <p className={style.title}>{title}</p>
-                  </div>
-              )
-          })
-
-      }
-      
-    </div>
-    )
 }
 export default Display
--- src/Containers/FetchData/FetchData.js
+++ src/Containers/FetchData/FetchData.js
@@ -1,31 +1,37 @@
-import React, {useState, useEffect} from 'react'
+import React, {PureComponent} from 'react'
 import Button from '../Guess-the-number/buttons'
 import Display from './DisplayData'
-import { useHistory } from "react-router-dom";
+import {withRouter} from 'react-router-dom'
 
-
-function Fetch(){
-    const history = useHistory();
-    const [posts, setPosts] =  useState([]);
-    useEffect(() => {
+class Fetch extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+        this.backToHome=this.backToHome.bind(this)
+    }
+  backToHome(){
+        this.props.history.push('/');
+  }
+  componentDidMount(){
+        console.log("qwe");
         fetch("https://jsonplaceholder.typicode.com/photos")
-            .then(res => res.json())
-            .then(data => setPosts(data))
-            .catch(error => console.log("error"))
-    })
-
-    const Home = ()=>{
-        history.push("/");
+        .then(res => res.json())
+        .then(data =>this.setState({data}))
+        .catch(error => console.log("error"))
+  }
+   render() {
+    const data = this.state.data;
+      return(
+        <div>
+             <div> 
+                 <Button props={{label: "Home", functions: this.backToHome,color:"primary"}} />
+            </div>
+            <Display props={data}/>
+        </div>
+      )
     }
 
-    return(
-    <div>   
-         <div>
-              <Button props={{label: "Home", functions: Home,color:"primary"}} />
-        </div>
-       < Display data={posts}/>
-      
-    </div>
-    )
 }
-export default Fetch
+  
+export default withRouter(Fetch)
+
