b1d927a4f9f6718f778d78e7b7643917ef828be6
SAMI KHAN <bcsf17a033@pucit.edu.pk>
2020-07-06 12:48:45 +0500

    Updating-Fetch-using-Redux

--- src/Containers/FetchData/FetchData.js
+++ src/Containers/FetchData/FetchData.js
@@ -20,30 +20,30 @@ class Fetch extends Component{
         const {fetchingGallery} = this.props
         fetchingGallery()
   }
-componentDidUpdate(prevProps){  
- if(prevProps.data !== this.props.data)
- {
-   this.setState({galleryData: [this.props.data]})
- }
-}
+  componentDidUpdate(prevProps){  
+      if(prevProps.data !== this.props.data)
+      {
+        this.setState({galleryData: [this.props.data]})
+      }
+  }
    render() {
-    if(this.state.galleryData[0] == undefined)
-    {
-     return(<Loading />)
-    }
-    else if(this.props.fetchingData == false && this.props.fetchError !== "")
-    {
-      return(<ErrorBar error={this.props.fetchError}/>)
-    }
-    return(
-        <div>
-             <div> 
-                 <Button btnData={{label: "Home", functions: this.backToHome,color:"primary"}} />
+        if(this.state.galleryData[0] == undefined)
+        {
+          return(<Loading />)
+        }
+        else if(this.props.fetchingData == false && this.props.fetchError !== "")
+        {
+          return(<ErrorBar error={this.props.fetchError}/>)
+        }
+        return(
+            <div>
+                <div> 
+                    <Button btnData={{label: "Home", functions: this.backToHome,color:"primary"}} />
+                </div>
+                <Display imgData={this.state.galleryData[0]}/>
             </div>
-            <Display imgData={this.state.galleryData[0]}/>
-        </div>
-      )
-    }
+          )
+        }
 }
 const mapStateToProps = (state) => {
   const {data, fetchingData, fetchError} = state.fetchData
